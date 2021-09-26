f333267ab90be91ed777db2f2d5d8d56233b0203
mementomori98 <sebestyen.mate98@gmail.com>
2021-09-26 17:17:12 +0200

    wip(diagram skeleton)

--- src/components/tests/Box.ts
+++ src/components/tests/Box.ts
@@ -1,10 +1,14 @@
 export class Box {
+    private static nextId: bigint = BigInt(0);
+
+    public id: bigint;
     public x: number;
     public y: number;
     public width: number;
     public height: number;
 
-    constructor(x: number, y: number, width: number, height: number) {
+    constructor(x: number, y: number, width: number, height: number, id: bigint = null) {
+        this.id = id ?? Box.nextId++;
         this.x = x;
         this.y = y;
         this.width = width;
