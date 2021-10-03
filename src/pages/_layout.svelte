515921162d55eb80447357317b3a9034b88ec877
Zoltan Vegh <zotyovegh@gmail.com>
2021-10-03 17:04:31 +0200

    VirtualSpace placed back

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -69,6 +69,9 @@
                 </ContextMenu>
             </svelte:fragment>
             <slot/>
+            {#if path !== '/editor' && path !== '/index'}
+                <VirtualSpace/>
+            {/if}
         </DrawerLayout>
         <Fab on:click={() => alert('fab clicked')}>
             <Icon icon="add"/>
