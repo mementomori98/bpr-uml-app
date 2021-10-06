d82a451bc22dd83fb4caf52645cd9dbbe97299c2
mementomori98 <sebestyen.mate98@gmail.com>
2021-10-06 14:46:02 +0200

    refactor login to use auth service

--- src/services/AuthenticationService.js
+++ src/services/AuthenticationService.js
@@ -1,14 +1,33 @@
-import "./Services";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 export class AuthenticationService {
-    login(username, password) {
-        localStorage.setItem('auth', 'true');
+    constructor() {
+        this.auth = getAuth();
+        this.tokenName = 'accessToken';
+    }
+    async login(username, password) {
+        try {
+            let res = await signInWithEmailAndPassword(this.auth, username, password);
+            localStorage.setItem(this.tokenName, await res.user.getIdToken());
+        }
+        catch (err) {
+            throw err;
+        }
+    }
+    async loginGoogle() {
+        try {
+            const provider = new GoogleAuthProvider();
+            let res = await signInWithPopup(this.auth, provider);
+            localStorage.setItem(this.tokenName, await res.user.getIdToken());
+        }
+        catch (err) {
+            throw err;
+        }
     }
     logout() {
-        localStorage.removeItem('auth');
+        localStorage.removeItem(this.tokenName);
     }
     isLoggedIn() {
-        return localStorage.getItem('auth') != null;
+        return localStorage.getItem(this.tokenName) != null;
     }
 }
-AuthenticationService.key = {};
 //# sourceMappingURL=AuthenticationService.js.map
--- src/services/AuthenticationService.ts
+++ src/services/AuthenticationService.ts
@@ -1,19 +1,37 @@
-import getService from "./Services";
+
+import {getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
 
 export class AuthenticationService {
 
-    public static readonly key = {}
+    private auth = getAuth();
+    private tokenName = 'accessToken';
 
-    public login(username: string, password: string) {
-        localStorage.setItem('auth', 'true');
+    public async login(username: string, password: string) {
+        try {
+            let res = await signInWithEmailAndPassword(this.auth, username, password);
+            localStorage.setItem(this.tokenName, await res.user.getIdToken())
+        }
+        catch (err) {
+            throw err;
+        }
+    }
+
+    public async loginGoogle() {
+        try {
+            const provider = new GoogleAuthProvider();
+            let res = await signInWithPopup(this.auth, provider)
+            localStorage.setItem(this.tokenName, await res.user.getIdToken());
+        } catch (err) {
+            throw err;
+        }
     }
 
     public logout() {
-        localStorage.removeItem('auth')
+        localStorage.removeItem(this.tokenName)
     }
 
     public isLoggedIn() {
-        return localStorage.getItem('auth') != null;
+        return localStorage.getItem(this.tokenName) != null;
     }
 
 }
