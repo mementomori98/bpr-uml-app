d9e4cadce05d38fe6910104ac7784d074d097440
mementomori98 <sebestyen.mate98@gmail.com>
2021-11-10 13:49:34 +0100

    aoeu

---
+++ src/components/diagrams/boxes/CreateBoxView.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+import View from "../../../ui/View.svelte";
+import Button from "../../../ui/Button.svelte";
+import Input from "../../../ui/Input.svelte";
+
+let text: string;
+
+</script>
+
+<View>
+    <svelte:fragment slot="header">Create Model</svelte:fragment>
+    <svelte:fragment slot="header-actions"/>
+        <Input label="Text" bind:value={text}/>
+    <svelte:fragment slot="actions">
+        <Button>Create</Button>
+        <Button>Cancel</Button>
+    </svelte:fragment>
+</View>
+
+<style lang="scss">
+    @import "../../../ui/theme.scss";
+
+</style>
