13c47f2a4871c692c7c491702ce704848aaa4c25
Zoltan Vegh <zotyovegh@gmail.com>
2021-11-15 00:41:25 +0100

    CardItem

--- src/components/projects/ProjectsView.svelte
+++ src/components/projects/ProjectsView.svelte
@@ -3,14 +3,22 @@
     import Wrapper from "../../ui/Wrapper.svelte";
     import ProjectUsersCard from "./ProjectUsersCard.svelte";
     import ProjectsCard from "./ProjectsCard.svelte";
+    import Card from "../../ui/Card.svelte";
+    import CardItem from "../../ui/CardItem.svelte";
+
+
 </script>
 
-<Container>
-    <Wrapper bgColor="ffffff">
-        <ProjectsCard/>
-    </Wrapper>
-</Container>
+<!--<Container>-->
+<!--    <Wrapper bgColor="ffffff">-->
+<!--        <ProjectsCard/>-->
+<!--    </Wrapper>-->
+<!--</Container>-->
+
+<CardItem title="Themis asd ahs dhas d asd"/>
 
 <style lang="scss">
   @import "../../ui/theme";
+
+
 </style>
---
+++ src/ui/CardItem.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+    import {CssBuilder} from "./utils/CssBuilder";
+    import {small} from "./MessageIcon.svelte";
+
+    export let style: string = '';
+    export let title: string = '';
+
+    $: itemClass = new CssBuilder('card-item')
+        .build();
+
+    $: iconStyle = new CssBuilder('icon')
+        .build();
+
+    const itemClick = () => {
+
+    }
+
+    const menuClick = (e) => {
+        e.stopPropagation()
+    }
+</script>
+
+<svelte:head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</svelte:head>
+
+<div class={itemClass} style={`${style}`} on:click={itemClick}>
+    <div class="title">{title}</div>
+
+    <i class="fa fa-ellipsis-v {iconStyle}"  on:click={menuClick}></i>
+</div>
+
+<style lang="scss">
+  @import "./theme";
+
+  .title{
+    font-weight: 700;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+
+  .card-item {
+    width: 200px;
+    min-height: 50px;
+    box-shadow: 0 0 10px 2px rgba(0, 0, 0, .1);
+    border-radius: 8px;
+    background-color: white;
+    margin-bottom: 32px;
+    padding: 15px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .icon{
+    font-size: 20px;
+    color: #5b5b5b;
+    align-self: center;
+    padding: 2px 10px 0 10px;
+    border-radius: 5px;
+    margin-left: 10px;
+
+    &:hover{
+      background-color: #ececec;
+      cursor: pointer;
+    }
+  }
+
+</style>
