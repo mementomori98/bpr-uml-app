7705549f8132d1189ec0afc76c493de62296b418
mementomori98 <sebestyen.mate98@gmail.com>
2021-11-26 12:00:00 +0100

    editor styling

--- src/ui/TreeView.svelte
+++ src/ui/TreeView.svelte
@@ -32,7 +32,8 @@
         border-radius: $border-radius-medium;
         background-color: #30476a;
         border: 1px solid #dfdfff4d;
-        overflow: auto;
+        overflow-y: auto;
+        overflow-x: auto;
         height: 100%;
         width: 100%;
         padding: 8px;
