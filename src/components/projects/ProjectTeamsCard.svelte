b052c90cffd228a511a1f687d69e858e92b44073
Zoltan Vegh <zotyovegh@gmail.com>
2021-11-26 10:01:00 +0100

    Display teams of project

--- src/components/projects/ProjectTeamsCard.svelte
+++ src/components/projects/ProjectTeamsCard.svelte
@@ -2,26 +2,21 @@
     import Card from "../../ui/Card.svelte";
     import View from "../../ui/View.svelte";
     import Button from "../../ui/Button.svelte";
-    import UserSettingsDialog from "../users/UserSettingsDialog.svelte";
-    import InviteProjectUserDialog from "../users/EditProjectUsersDialog.svelte";
-    import Checkbox from "../../ui/Checkbox.svelte";
-    import CloseButton from "../../ui/CloseButton.svelte";
     import ListRowItem from "../../ui/ListRowItem.svelte";
     import ListRow from "../../ui/ListRow.svelte";
     import {goto, params} from "@roxi/routify";
-    import {Team, TeamResponse} from "../teams/Models";
-    import EditTeamsDialog from "../teams/EditTeamsDialog.svelte";
+    import {Team} from "../teams/Models";
+    import EditProjectTeamsDialog from "../teams/EditTeamsDialog.svelte";
     import TeamSettingsDialog from "../teams/TeamSettingsDialog.svelte";
     import ListScrollWrapper from "../../ui/ListScrollWrapper.svelte";
     import {onMount} from "svelte";
     import {sortUserList} from "../../ui/utils/ListItem";
     import getService from "../utils/ServiceFactory";
-    import {UserService} from "../users/UserService";
-    import {AppContext} from "../utils/AppContext";
-    import {TeamService} from "../teams/TeamService";
+    import {ProjectService} from "./ProjectService";
+    import {ProjectResponse} from "./Models";
 
-    const teamsService = getService(TeamService);
-    const appContext = getService(AppContext);
+    const projectService = getService(ProjectService);
+    let project: ProjectResponse = new ProjectResponse({title: "", users: [], teams: [], _id: "", workspaceId: ""});
 
     let editVisible: boolean = false;
     let readVisible: boolean = false;
@@ -29,10 +24,15 @@
     let teams = []
 
     onMount(async () => {
-       // teams = await teamsService.getProjectTeams($params.id)
+        project = await projectService.getProject($params.id)
+        project.teams = sortUserList(project?.teams);
     })
 
-    const handleClick = (team) => {
+    const onEdit = async () => {
+        project = await projectService.getProject($params.id)
+    }
+
+    const handleReadingTeam = (team) => {
         readVisible = true
         chosenTeam = team
     }
@@ -51,20 +51,20 @@
                 </ListRow>
             </svelte:fragment>
             {#each teams as team}
-                <ListRow on:click={() => handleClick(team)}>
+                <ListRow on:click={() => handleReadingTeam(team)}>
                     <ListRowItem widthInPercentage={72}>{team.name}</ListRowItem>
                     <ListRowItem center widthInPercentage={14}>{team.usersAmount}</ListRowItem>
                     <ListRowItem center widthInPercentage={14}>{team.projectsAmount}</ListRowItem>
                 </ListRow>
             {/each}
         </ListScrollWrapper>
-        <svelte:fragment slot="actions"> <!-- TODO disabled if not product owner-->
+        <svelte:fragment slot="actions">
             <Button on:click={() => editVisible = true}>Edit</Button>
         </svelte:fragment>
     </View>
 </Card>
 
-<EditTeamsDialog bind:visible={editVisible}/>
+<EditProjectTeamsDialog bind:visible={editVisible} on:edit={onEdit}/>
 <TeamSettingsDialog readonly bind:visible={readVisible} teamName={chosenTeam === null ? "" : chosenTeam.name}/>
 
 <style lang="scss">
--- src/components/projects/ProjectUsersCard.svelte
+++ src/components/projects/ProjectUsersCard.svelte
@@ -2,20 +2,17 @@
     import Card from "../../ui/Card.svelte";
     import View from "../../ui/View.svelte";
     import Button from "../../ui/Button.svelte";
-    import UserSettingsDialog from "../users/UserSettingsDialog.svelte";
     import EditProjectUsersDialog from "../users/EditProjectUsersDialog.svelte";
     import Checkbox from "../../ui/Checkbox.svelte";
-    import CloseButton from "../../ui/CloseButton.svelte";
     import ListRowItem from "../../ui/ListRowItem.svelte";
     import ListRow from "../../ui/ListRow.svelte";
     import {goto, params} from "@roxi/routify";
     import ListScrollWrapper from "../../ui/ListScrollWrapper.svelte";
-    import {User} from "../users/Models";
     import {onMount} from "svelte";
     import {ProjectResponse} from "./Models";
     import getService from "../utils/ServiceFactory";
     import {ProjectService} from "./ProjectService";
-    import {sortList, sortUserList} from "../../ui/utils/ListItem";
+    import {sortUserList} from "../../ui/utils/ListItem";
 
     const projectService = getService(ProjectService);
     let project: ProjectResponse = new ProjectResponse({title: "", users: [], teams: [], _id: "", workspaceId: ""});
@@ -54,7 +51,7 @@
                 </ListRow>
             {/each}
         </ListScrollWrapper>
-        <svelte:fragment slot="actions"> <!-- TODO disabled if not product owner-->
+        <svelte:fragment slot="actions">
             <Button on:click={() => editVisible = true}>Edit</Button>
         </svelte:fragment>
     </View>
